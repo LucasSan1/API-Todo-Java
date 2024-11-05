20940d684af57c4a2d13067c7cf09a70cdbb45fc
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2024-11-05 16:54:50 -0300

    Add files via upload

---
+++ desafio-todolist/src/main/java/com/dstodo/desafio_todolist/DesafioTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.dstodo.desafio_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTodolistApplication.class, args);
+	}
+
+}
