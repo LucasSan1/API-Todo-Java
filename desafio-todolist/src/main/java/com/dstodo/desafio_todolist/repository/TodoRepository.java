1db950aa7e2090439b10756314188a117872eb6a
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2024-11-11 14:56:12 -0300

    Delete desafio-todolist/src directory

--- desafio-todolist/src/main/java/com/dstodo/desafio_todolist/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dstodo.desafio_todolist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dstodo.desafio_todolist.entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-
-}
